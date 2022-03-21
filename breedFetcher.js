fee8e2f6f25786ae63b0bd95b357ee1dfeec4050
Derek Harvey <derek.harvey0422@gmail.com>
2022-03-21 23:32:53 +0000

    added tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,25 @@
 const request = require("request");
 
-let breed = process.argv[2];
+
 
 
 const fetchBreedDescription = function(breed, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-// edge case if there is an error
-  if (error) {
-    callback(error);
-  } else {
-  //console.log(typeof body);
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    error = "We were unable to find the breed that you requested. Please double check for typos!";
-    callback(error);
-  } else {
-    callback(error, data[0].description);
-  }
-}
-});
-}
+    // edge case if there is an error
+    if (error) {
+      callback(error);
+    } else {
+      //console.log(typeof body);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        error = "We were unable to find the breed that you requested. Please double check for typos!";
+        callback(error);
+      } else {
+        callback(error, data[0].description);
+      }
+    }
+  });
+};
 module.exports = {fetchBreedDescription};
 
 
