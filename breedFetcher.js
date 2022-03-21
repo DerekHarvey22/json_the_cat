a4cbdcf20b8f6a1a3f92e56f17beaf9d318d7780
Derek Harvey <derek.harvey0422@gmail.com>
2022-03-21 22:27:59 +0000

    prints description of breed

--- breedFetcher.js
+++ breedFetcher.js
@@ -7,5 +7,6 @@ request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, respons
   //console.log(typeof body);
   const data = JSON.parse(body);
   console.log(data);
-  console.log(typeof data);
+  //access the first entry in data array and print out the description for the user
+  console.log(data[0].description);
 })
