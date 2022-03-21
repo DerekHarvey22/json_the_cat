827bd0beaf5fccb0c9be0e5d9ae97b4aa808cb57
Derek Harvey <derek.harvey0422@gmail.com>
2022-03-21 22:53:26 +0000

    added edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,14 +5,15 @@ let breed = process.argv[2];
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
 // edge case if there is an error
-if (error) {
-  console.log(error, `${breed} returned an error. Doublecheck your spelling or reaffrim that the breed you are looking for exists!`)
-}  
-
-
-//console.log(typeof body);
+  if (error) {
+    console.log(error);
+    return;
+  }
+  //console.log(typeof body);
   const data = JSON.parse(body);
-  console.log(data);
-  //access the first entry in data array and print out the description for the user
-  console.log(data[0].description);
-})
+  if (data.length === 0) {
+    console.log("We were unable to find the breed that you requested. Please double check for typos!");
+  } else {
+    console.log(data[0].description);
+  }
+});
